a2a598124c338a5f5ec6f555017b3263c0fc45e7
skyler <121411614+han-skyler@users.noreply.github.com>
2025-09-09 20:07:02 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int buta_year;
+    cin >> buta_year;
+    if (1000 <= buta_year && buta_year <= 3000) {
+        cout << buta_year - 543 << endl;
+    }
+    return 0;
+}
