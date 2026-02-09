c43abfe609b9f73e08162a1aa66ce6abd8eacba8
한지윤 <121411614+han-skyler@users.noreply.github.com>
2026-02-10 00:13:51 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i+1; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	return 0;
+}
