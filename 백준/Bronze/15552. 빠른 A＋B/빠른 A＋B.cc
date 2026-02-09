c7602216f7d7495161c1dad9bf485a3bd9a2c6c6
한지윤 <121411614+han-skyler@users.noreply.github.com>
2026-02-10 00:22:06 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 220 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n;
+	int a, b;
+
+	std::cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		std::cin >> a >> b;
+		std::cout << a + b << "\n";
+	}
+
+	return 0;
+}
