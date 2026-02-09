e9415c434964fd05327e46861f8ade44481e9512
한지윤 <121411614+han-skyler@users.noreply.github.com>
2026-02-10 00:20:55 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	char id[51];
+
+	cin >> id;
+	cout << id << "??!";
+
+	return 0;
+}
