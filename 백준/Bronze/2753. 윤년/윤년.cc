203d1cde9c80627654bc132cdac307e1e78be5a5
한지윤 <121411614+han-skyler@users.noreply.github.com>
2026-02-10 00:16:57 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+	int year = 0;
+	scanf("%d", &year);
+
+	if (year % 4 == 0)
+	{
+		if (year % 400 == 0)
+		{
+			if (year % 100 != 0) printf("1");
+			else printf("1");
+		}
+		else if (year % 100 == 0) printf("0");
+
+		else printf("1");
+	}
+
+	else printf("0");
+
+	return 0;
+}
